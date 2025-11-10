769ab42363f83f9634c120b87c80d93821dc4339
Lokesh <lokeshmanideep14@gmail.com>
2025-11-10 00:49:55 -0600

    fix reloading

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Navigate, Route, HashRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
 import About from './components/pages/About';
 import Certifications from './components/pages/Certifications';
